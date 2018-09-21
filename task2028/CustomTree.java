41a7c4837c6e7200604ec248a1482b22fd884c21
Denis <tapfereherz@gmail.com>
2018-09-22 00:37:36 +0700

    Реализованы методы добавления нового элемента,  вывода имени родительского узла и подсчет количества элементов в дереве

--- task2028/CustomTree.java
+++ task2028/CustomTree.java
@@ -2,6 +2,7 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -9,9 +10,107 @@ import java.util.List;
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements  Cloneable, Serializable {
-    public Entry<String> root;
+    Entry<String> root;
+    Entry<String> lastEntry;
+    ArrayList<Entry<String>> innerList = new ArrayList<Entry<String>>();
+    //конструктор множества, создание корня
+    public CustomTree(){
+        this.root = new Entry<String>("0");
+
+        this.root.parent =null;
+        this.root.lineNumber = 0;
+        innerList.add(0, root);
+
+    }
 
 
+    //метод добавления нового элемента
+    @Override
+    public boolean add( String element){
+
+        // создаем объект класса Entry с параметром element
+        lastEntry = new Entry<String>(element);
+
+        //перед добавлением новогоэлемента проверяем наличие корня дерева
+        if(root.equals(null)){
+
+            //передаем ссылку на новый объект
+            root = lastEntry;
+
+            //проверяем возможность добавлять новые ветви
+            root.isAvailableToAddChildren();
+
+            //добавляем корень на первую позицию в множество
+            innerList.add(0, root);
+            root.lineNumber = 0;
+
+        }
+        else{
+
+            // необходимо добавить перебор элементов дерева, у которых уже заняты ветви
+            for(Entry<String> currentEntry : innerList ) {
+                if( currentEntry.isAvailableToAddChildren()) {
+
+
+                    //добавляем новый элемент слева, если левое направление свободно
+                    if(currentEntry.availableToAddLeftChildren) {
+
+
+                            //добавляем новый элемент в общий внутренний список
+                            innerList.add((currentEntry.lineNumber * 2) + 1,lastEntry);
+
+                            // связываем родительский узел с новым узлом
+                            currentEntry.leftChild = lastEntry;
+                            lastEntry.parent = currentEntry;
+
+                            //добавляем линейный номер в новый элемент
+                            lastEntry.lineNumber = (currentEntry.lineNumber * 2) + 1;
+
+
+
+                            // указываем на возможность добавления  новых узлов на дереве у нового элемента
+
+                        currentEntry.availableToAddLeftChildren = false;
+                            return true;
+                    }
+                    //добавляем новый элемент справа
+                    else{
+
+                        // связываем родительский узел с новым узлом
+                        currentEntry.rightChild = lastEntry;
+                        lastEntry.parent = currentEntry;
+
+                        //добавляем линейный номер в новый элемент
+                        lastEntry.lineNumber = (currentEntry.lineNumber * 2) + 2;
+
+                        //добавляем новый элемент в общий внутренний список
+                        innerList.add((currentEntry.lineNumber * 2) + 2, lastEntry);
+
+                        // указываем на возможность добавления  новых узлов на дереве у нового элемента
+                        currentEntry.checkChildren();
+                        return true;
+                    }
+                }
+            }
+       }
+        return false;
+    }
+
+
+    public String getParent(String name) {
+        // перебираем элементы нашего множества
+        for(Entry<String> currentEntry : innerList ) {
+
+            //проверяем - есть ли среди них тот, имя которого совпадает с искомым
+            if( currentEntry.elementName.equals(name)) {
+                //возвращаем имя родительского узла
+                return currentEntry.parent.elementName;
+            }}
+
+            //если родительского узла нет - null
+        return null;
+
+    }
 
     @Override
     public String get(int index) {
@@ -21,17 +120,22 @@ public class CustomTree extends AbstractList<String> implements  Cloneable, Seri
 
     @Override
     public int size() {
-        return 0;
+        int size = 0;
+        for(Entry<String> currentEntry : innerList ) {
+            if( !currentEntry.equals(null)) {
+                size++;
+            }}
+        return size - 1;
     }
+
+
+
     @Override
     public String set(int index, String element){
         throw new UnsupportedOperationException();
     }
 
-    @Override
-    public void add(int index, String element){
-        throw new UnsupportedOperationException();
-    }
+
 
     @Override
     public String remove(int index){
@@ -67,10 +171,10 @@ public class CustomTree extends AbstractList<String> implements  Cloneable, Seri
 
         }
         public void checkChildren(){
-            if (this.leftChild.equals(null)){
+            if (!(leftChild.equals(null))){
                 this.availableToAddLeftChildren = false;
             }
-            if(this.rightChild.equals(null)){
+            if(!(rightChild.equals(null))){
                 this.availableToAddRightChildren = false;
             }
         }
