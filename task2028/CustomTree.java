aefed0759bb76a8425f584582519eab1cc709140
Denis <tapfereherz@gmail.com>
2018-09-21 15:08:23 +0700

    Добавлен внутренний класс Entry, реализованы его методы( логические функции - изменение значений флагов , указывающих на возможность добавления новых ветвей дерева), добавлены статические поля

--- task2028/CustomTree.java
+++ task2028/CustomTree.java
@@ -2,20 +2,82 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
 
 /* 
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements  Cloneable, Serializable {
+    public Entry<String> root;
+
+
 
     @Override
     public String get(int index) {
-        return null;
+        throw new UnsupportedOperationException();
+
     }
 
     @Override
     public int size() {
         return 0;
     }
+    @Override
+    public String set(int index, String element){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c){
+        throw new UnsupportedOperationException();
+    }
+
+
+     static class Entry<T> implements Serializable {
+        public String elementName;
+        public int lineNumber;
+        public boolean availableToAddLeftChildren, availableToAddRightChildren;
+        public Entry<T> parent, leftChild, rightChild;
+
+        public Entry(String elementName){
+            this.elementName = elementName;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+
+        }
+        public void checkChildren(){
+            if (this.leftChild.equals(null)){
+                this.availableToAddLeftChildren = false;
+            }
+            if(this.rightChild.equals(null)){
+                this.availableToAddRightChildren = false;
+            }
+        }
+        public boolean isAvailableToAddChildren(){
+            return (availableToAddLeftChildren|availableToAddRightChildren);
+        }
+
+    }
+
 }
